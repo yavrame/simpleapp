49978c957b1faa368b36ba08916b564f4be65628
yar <yavrame@gmail.com>
2024-06-03 22:01:21 +0300

    simpleapp init

---
+++ src/main/java/com/example/simpleapp/SimpleappApplication.java
@@ -0,0 +1,14 @@
+package com.example.simpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleappApplication.class, args);
+	}
+
+
+}
